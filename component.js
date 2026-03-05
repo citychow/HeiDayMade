38ef334c29d62e5f0b55164e19252777a9b80ecf
citychow <citychow@connect.hku.hk>
2026-03-05 16:59:18 +0000

    Update content and add gif

--- component.js
+++ component.js
@@ -43,13 +43,13 @@ export const Navbar = (currentPage) =>
 export const Footer = () => `
       <div class="container">
           <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-            <p class="col-md-4 mb-0 text-body-secondary">©HeiDayMade ${new Date().getFullYear()}</p>
+            <p class="col-lg-4 mb-0 text-body-secondary">©HeiDayMade ${new Date().getFullYear()}</p>
 
-            <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none" aria-label="Bootstrap">
+            <a href="/" class="col-lg-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none" aria-label="Bootstrap">
               <svg class="bi me-2" width="40" height="32" aria-hidden="true"><use xlink:href="#bootstrap"></use></svg>
             </a>
         
-            <ul class="nav col-md-4 justify-content-end">
+            <ul class="nav col-lg-4 justify-content-end">
               <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
               <li class="nav-item"><a href="./About-me.html" class="nav-link px-2 text-body-secondary">About</a></li>
               <li class="nav-item"><a href="./Gallery-Main.html" class="nav-link px-2 text-body-secondary">Gallery</a></li>
