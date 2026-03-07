d0404745c0dbfd353d4b575712bffe9b02bc10dd
citychow <citychow@connect.hku.hk>
2026-03-07 11:07:02 +0000

    Fix Footer md to lg and update About link in navbar and footer

--- component.js
+++ component.js
@@ -45,7 +45,7 @@ export const Footer = () => `
           <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
             <p class="col-lg-4 mb-0 text-body-secondary">©HeiDayMade ${new Date().getFullYear()}</p>
 
-            <a href="/" class="col-lg-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none" aria-label="Bootstrap">
+            <a href="/" class="col-lg-4 d-flex align-items-center justify-content-center mb-3 mb-lg-0 me-lg-auto link-body-emphasis text-decoration-none" aria-label="Bootstrap">
               <svg class="bi me-2" width="40" height="32" aria-hidden="true"><use xlink:href="#bootstrap"></use></svg>
             </a>
         
